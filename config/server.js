e56663780a60879b829b08b7ae777ccddff382cb
dario alejandro <darioalejandrovv@gmail.com>
2020-11-26 18:05:43 -0500

    First up

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c9031d9f924583002ce7b632a77336f5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a01eba44-22d6-4fa3-a8a6-a38380b07ddd'
+};
